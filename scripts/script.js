44d6d426e4490c0b343cc42a0839ded5dd7c2877
stefanappdev <stevendking@protonmail.com>
2026-09-24 13:47:36 -0700

    finished hamburger mneu for mobile

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+let dropdownMenuBtn=document.querySelector('#dropdown-menu-icon')
+
+
+function openMenu(){
+    let dropDownContent=document.querySelector('.dropdown-content');
+    if(dropDownContent.classList.contains('hide')){
+        dropDownContent.classList.replace('hide','show')
+    } 
+}
+
+function closeMenu(){
+     
+     let dropDownContent=document.querySelector('.dropdown-content');
+    if(dropDownContent.classList.contains('show')){
+         dropDownContent.classList.replace('show','hide')
+            
+    }
+}
